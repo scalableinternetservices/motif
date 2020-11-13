531f8e4144a0ba20a8b41b33a078b231db44b274
Alan Guan <alanguan98@gmail.com>
2020-11-13 13:54:47 -0800

    Merge Alan's user query

--- web/src/view/page/UserLogin/UserLogin.tsx
+++ web/src/view/page/UserLogin/UserLogin.tsx
@@ -1,23 +1,54 @@
 import { RouteComponentProps } from '@reach/router';
 import * as React from 'react';
+import { check } from '../../../../../common/src/util';
 import { Button } from '../../../style/button';
 import { Input } from '../../../style/input';
 import { link } from '../../nav/Link';
 import { AppRouteParams } from '../../nav/route';
 import { Page } from '../../page/Page';
+import { toastErr } from '../../toast/toast';
 
 
 interface UserLoginProps extends RouteComponentProps, AppRouteParams {}
 
 export function UserLogin (p: UserLoginProps) {
   const [userName, setUser] = React.useState("");
+  const [err, setError] =  React.useState({ userName: false })
+
+  // reset error when email/name change
+
+  React.useEffect(() => setError({ ...err, userName: false }), [userName])
+
+  function login() {
+    if (!validate(userName, setError)) {
+      toastErr('name')
+      return
+    }
+
+    fetch('/auth/createUser_', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ userName }),
+    })
+      .then(res => {
+        check(res.ok, 'response status ' + res.status)
+        return res.text()
+      })
+      .then(() => window.location.replace('/'))
+      .catch(err => {
+        toastErr(err.toString())
+        setError({ userName: true })
+      })
+  }
+
+
 
   return (
     <Page>
       <div className="baseCanvas bg-green">
         <div className="flex flex-column justify-center pa6">
           <UserNameField setUser={setUser}/>
-          <SubmitUser userName={userName}/>
+          <SubmitUser login={()=> login()} userName={userName}/>
         </div>
       </div>
     </Page>
@@ -39,12 +70,13 @@ function UserNameField (p : UserNameProps)
 
 interface SubmitUserProps {
   userName : string,
+  login(): void
 }
 
 function SubmitUser(p: SubmitUserProps) {
   return (
     <div className="mh4 mv2 flex justify-center">
-      <ButtonLink onClick={ () => {alert("Submitting User: " + p.userName)} }>
+      <ButtonLink onClick={p.login}>
         Submit
       </ButtonLink>
 
@@ -53,3 +85,14 @@ function SubmitUser(p: SubmitUserProps) {
 }
 
 const ButtonLink = link(Button)
+
+
+function validate(
+  userName: string,
+  setError: React.Dispatch<React.SetStateAction<{ userName: boolean }>>
+) {
+  const validName = Boolean(userName)
+  console.log('valid',  validName)
+  setError({ userName: !validName })
+  return validName
+}
